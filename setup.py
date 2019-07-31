763530421ff666ab75c40583901d838356f9a1a5
birkin <birkin.diana@gmail.com>
2019-07-31 15:48:21 -0400

    updates version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='patron-api',
-    version='0.3-dev',
+    version='0.4',
     packages=find_packages(),
     install_requires=[ 'requests' ],
 )
